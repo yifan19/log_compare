0d0a2101362bf15dcbcfa0b2d340f8602c8f1cd7
Yi Fan Yu <yiyu.yu@mail.utoronto.ca>
2023-06-26 07:08:38 +0000

    reconstruct byte py

--- python/nyte2.py
+++ python/nyte2.py
@@ -85,6 +85,27 @@ def parse_instruction(instruction):
         if argument_match:
             result["argument"] = argument_match.group(2)
         return result
+        
+    elif "Print or use an existing stacktrace" in instruction:
+        function_match = re.search(r"caller function of(.+)_(.+)_(.+)", instruction)
+        result = {
+            "id": "stack trace",
+            "type": "stack_trace",
+            "function": function_match.group(2),
+            "class": function_match.group(1),
+            "suffix": function_match.group(3),
+            "loop": False
+        }
+        return result
+    elif "Accessing field member" in instruction:
+        class_match = re.search(r"Accessing field member of a class(.+). So print variable", instruction)
+        result = {
+            "id": "access field",
+            "type": "print_var",
+            "class": class_match.group(1),
+            "loop": False
+        }
+        return result
 
     return None
 
@@ -117,6 +138,8 @@ def to_byteman_rule(instruction):
         print_statement += f' + (${instruction["argument"]})'
     print_statement += ')'
 
+    if rule_type == "stack_trace":
+        print_statement = 'traceln("[BM][" + Thread.currentThread().getStackTrace()[5] + "][Stack Trace]");\n   traceln("[BM][" + Thread.currentThread().getStackTrace()[6] + "][Stack Trace]")'
 
     rule = f'''RULE ID {rule_id}
 CLASS {class_name}
@@ -130,102 +153,7 @@ ENDRULE
     return rule
 def print_var(instructions, p0):
     print("HERE")
-
-def get_byteman_entries(methods):
-    rules = []
-    for method in methods:
-        if method not in function_signatures:
-            print("signature not found: ", method)
-            continue
-        signature = function_signatures[method]
-        rule = f'''RULE entering {method}
-CLASS {function_classes[signature]}
-METHOD {signature}
-COMPILE
-AT ENTRY
-IF true
-DO traceStack("[BM][" + Thread.currentThread().getName() + "][Method Entry]");
-ENDRULE
-'''
-        rules.append(rule)
-    return rules 
-
-def get_byteman_stack(function, class_name, suffix):
-    print_statement = 'traceln("[BM][" + Thread.currentThread().getStackTrace()[5] + "][Stack Trace]");\n   traceln("[BM][" + Thread.currentThread().getStackTrace()[6] + "][Stack Trace]")'
-    signature = function_signatures[function]
-    rule = f'''RULE stack trace {function}_{suffix}
-CLASS {function_classes[signature]}
-METHOD {signature}
-COMPILE
-AT ENTRY
-IF true
-DO {print_statement}
-ENDRULE
-'''
-    return rule
-def get_byteman_field(branch, class_name):
-    function = "chooseRandom"
-    signature = function_signatures[function]
-    class_name = function_classes[signature]
-    var = "blocksize"
-    print_statement = f'traceln("[BM][" + Thread.currentThread().getName() + "][Field Member]," + (${var}) )'
-    rule = f'''RULE print var {function}: {var}
-CLASS {class_name}
-METHOD {signature}
-COMPILE
-AT ENTRY
-IF true
-DO {print_statement}
-ENDRULE
-'''
-    return rule, function
     
-def process_branch(branch, branch_id):
-    rules = []
-    result = {
-        "type": 0, # DIV
-        "id": branch_id
-    }
-    if "Print or use an existing stacktrace" in branch:
-        result["type"] = 1 # Stack
-        function_match = re.search(r"caller function of(.+)_(.+)_(.+)", branch)
-        function = function_match.group(2)
-        class_name = function_match.group(1),
-        suffix = function_match.group(3),
-        rules.append(get_byteman_stack(function, class_name, suffix))
-        result["function"] = function     
-    elif "Accessing field member" in branch:
-        result["type"] = 2 # ARG
-        class_match = re.search(r"Accessing field member of a class(.+). So print variable", branch)
-        class_name = class_match.group(1),
-        rule, function = get_byteman_field(branch, class_name)
-        rules.append(rule)
-        result["function"] = function
-    
-    instructions = branch.split("\n\n")
-    parsed = []
-    methods = set()
-    for instruction in instructions:
-        instruction = instruction.strip("\n")
-        p = parse_instruction(instruction)
-        if p is None:
-            continue
-
-        parsed.append(p) # do not append if is print_var
-        methods.add(p["function"])            
-        print(p)
-
-    if result["type"] == 0:
-        rules += get_byteman_entries(methods)
-        
-    for p in parsed:
-        rule = to_byteman_rule(p)
-        rules.append(rule)
-        print(rule)
-    
-    result["rules"] = rules
-    return result
-
 def process_command(content):
     match = re.search(r'(Branch ID: )?(.*?)(?=What is the ID to continue|$)', content, re.DOTALL)
     if match:
@@ -235,21 +163,67 @@ def process_command(content):
     print("\n\n////////////////////////\n\n")
     i = 0
     
-    processed = {}
+    b_types = {}
+    b_rules = {}
     for b in branches:
-        branch_match = re.search(r"Branch ID: (\d+)", b)
-        print(b, "!!!!!!!")
-        if branch_match:
-            branch_id = branch_match.group(1)
-        else:
+        branch_type = 0 # DIV
+        print("THERE")
+        instructions = b.split("\n\n")
+        branch_match = re.search(r"Branch ID: (\d+)", instructions[0])
+        print(b)
+        print("!!!!!!!")
+        if not branch_match:
             if i != 0:
                 continue
             else:
-                branch_id = "0" 
+                branch_id = "0"
+        else:
+            branch_id = branch_match.group(1)
         i += 1
         print("Branch ID is", branch_id, "///////")
-        p = process_branch(b, branch_id)
-        rules = p["rules"]
+        parsed = []
+        methods = set()
+        for instruction in instructions:
+            instruction = instruction.strip("\n")
+            # if not instruction.startswith("ID: "):
+                #
+            p = parse_instruction(instruction)
+            if p is None:
+                continue
+            if p["type"] == "print_var":
+                branch_type = 0
+                print_var(instructions, p)
+                continue
+            parsed.append(p) # do not append if is print_var
+            methods.add(p["function"])
+            if p["type"] == "stack_trace":
+                branch_type = 1
+                break
+            
+            # print(p)
+
+        rules = []
+        if branch_type == 0:
+            for method in methods:
+                if method not in function_signatures:
+                    print("signature not found: ", method)
+                    continue
+                function = function_signatures[method]
+                rule = f'''RULE entering {method}
+CLASS {function_classes[function]}
+METHOD {function}
+COMPILE
+AT ENTRY
+IF true
+DO traceStack("[BM][" + Thread.currentThread().getName() + "][Method Entry]");
+ENDRULE
+'''
+                rules.append(rule)
+                print(rule)
+        for p in parsed:
+            rule = to_byteman_rule(p)
+            rules.append(rule)
+            print(rule)
         file_name = btm_path + '/current_b' + branch_id + '.btm'
         print("write to:", file_name)
         with open(file_name, 'w') as f:
@@ -257,8 +231,9 @@ def process_command(content):
                 print(rule)
                 f.write(rule)
                 f.write('\n')
-        processed[branch_id] = p
-    return processed
+        b_types[branch_id] = branch_type
+        b_rules[branch_id] = parsed
+    return b_rules, b_types
 
 
 def find_between(file_path, start_pos):
@@ -292,16 +267,16 @@ def run():
             for root, dirs, files in os.walk(btm_path, topdown=False):
                 for file in files:
                     print("removing", file)
-                    os.remove(os.path.join(root, file)) # remove old btm
-            branches = process_command(content) # generate btm
+                    os.remove(os.path.join(root, file))
+            b_rules, b_types = process_command(content) # generate btm
             os.chdir("/home/ubuntu/hadoop/")
             for root, dirs, files in os.walk(log_path, topdown=False):
                 for file in files:
                     print("removing", file)
-                    os.remove(os.path.join(root, file)) # remove old logs
-            for b_id in branches.keys():
-                subprocess.run(['bash', bash_path], input=b_id, text=True)
-                print("bash done:", b_id)
+                    os.remove(os.path.join(root, file))
+            for b in b_rules.keys():
+                subprocess.run(['bash', bash_path], input=b, text=True)
+                print("bash done:", b)
             os.chdir('/home/ubuntu/log/log_compare/')
             log_files = glob.glob(os.path.join(log_path, "current_b*"))
             results = {}
@@ -313,23 +288,35 @@ def run():
                 branch_id = re.search(r"current_b(\d+).log", log).group(1)
                 print("///////////////////////////////////////////////////")
                 print("B id:", branch_id)
-                branch = branches[branch_id]
-                branch_type = branch["type"]
+                branch_type = b_types[branch_id]
+                parsed = b_rules[branch_id]
+
+                # print("branch types:", b_types)
+                # print("rules ", b_rules)
+                # print("parsed", parsed)
                 
                 if branch_type == 0:
                     result = subprocess.run([compare, log, '0'], capture_output=True, text=True)
                     results[branch_id] = result
                 elif branch_type == 1:
-                    function = branch["function"]
-                    result = subprocess.run([compare, log, '1', function], capture_output=True, text=True)
-                    results[branch_id] = result
-                elif branch_type == 2:
-                    function = branch["function"]
-                    result = subprocess.run([compare, log, '0'], capture_output=True, text=True)
-                    results[branch_id] = result
+                    function = ''
+                    for p in parsed:
+                        print("p: " , p)
+                        print(p["type"])
+                        if p["type"] == 'stack_trace':
+                            function = p["function"]
+                            result = subprocess.run([compare, log, '1', function], capture_output=True, text=True)
+                            results[branch_id] = result
+                            break
+                    if function == '':
+                        print("panick")
                 print("Output:", result.stdout)
                 print( result.stderr)
-
+                # os.remove(log)
+            btm_files = glob.glob(os.path.join(btm_path, "current_b*"))
+            for btm in btm_files:
+                print("remove", btm)
+                # os.remove(btm)
             print("------")
             parse_results(results)
             print('\nWaiting for instructions\n')
